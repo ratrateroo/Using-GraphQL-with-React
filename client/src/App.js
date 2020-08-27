c5e8a96545c54144e167f2660a0f10c7a1a97edb
Mark Leo <ratrateroo@gmail.com>
2020-08-27 20:17:16 +0800

    added apollo client and provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 
 import BookList from "./components/BookList";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
+
 const App = () => {
   return (
-    <div>
-      <h1>Let's Work</h1>;
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Let's Work</h1>;
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
