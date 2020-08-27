adb73807cf1b53efd4b4964ec566e1a9500d53b4
Mark Leo <ratrateroo@gmail.com>
2020-08-27 19:51:40 +0800

    setup the frontend

---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <h2>BookList</h2>
+      <ul>
+        <li>Book1</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
