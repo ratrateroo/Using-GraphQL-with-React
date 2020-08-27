d93de9f6dafeeb8a1f652e7fd36bf3c9901a7fbb
Mark Leo <ratrateroo@gmail.com>
2020-08-27 22:35:13 +0800

    displayed data correctly

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -11,15 +11,23 @@ const getBooksQuery = gql`
 `;
 
 const BookList = (props) => {
-  const { loading, error, data } = useQuery(getBooksQuery);
-  console.log(loading, error, data);
+  const { loading, error, data, books } = useQuery(getBooksQuery);
+  console.log(loading, error, data, books);
+
+  const displayBooks = () => {
+    if (loading) {
+      return <h2>Loading Books</h2>;
+    } else {
+      return data.books.map((book) => {
+        return <li>{book.name}</li>;
+      });
+    }
+  };
 
   return (
     <div>
       <h2>BookList</h2>
-      <ul>
-        <li>Book1</li>
-      </ul>
+      <ul>{displayBooks()}</ul>
     </div>
   );
 };
