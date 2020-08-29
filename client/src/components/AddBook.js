7afd1e00b698aab515c3524111885c2fba9647fc
Mark Leo <ratrateroo@gmail.com>
2020-08-29 09:56:10 +0800

    added component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import { getAuthorsQuery } from "../queries/queries";
 
 const AddBook = (props) => {
+  const [name, setName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const genreChangeHandler = (e) => {
+    setGenre(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const authorChangeHandler = (e) => {
+    setAuthor(e.target.value);
+    console.log(e.target.value);
+  };
+
   const { loading, data } = useQuery(getAuthorsQuery);
   const displayAuthors = () => {
     if (loading) {
@@ -27,15 +46,15 @@ const AddBook = (props) => {
     <form id='add-book'>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input type='text' onChange={nameChangeHandler} />
       </div>
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input type='text' onChange={genreChangeHandler} />
       </div>
       <div className='field'>
         <label>Author:</label>
-        <select>
+        <select onChange={authorChangeHandler}>
           <option>Select author</option>
           {displayAuthors()}
         </select>
